d4b16a959175f56bb7b529d8da93166b1dca7aa1
Shunki Fujiwara <shunki-fujiwara@freee.co.jp>
2022-07-28 23:01:06 +0900

    abc144 b.go

---
+++ abc/abc144/b.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var reader = NewReader()
+
+func main() {
+	in := reader.Int()
+
+	for i := 1; i <= 9; i++ {
+		for j := 1; j <= 9; j++ {
+			if i*j == in {
+				fmt.Println("Yes")
+				os.Exit(0)
+			}
+		}
+	}
+	fmt.Println("No")
+}
+
+// Read
+type Reader struct {
+	r *bufio.Reader
+}
+
+func NewReader() Reader {
+	return Reader{bufio.NewReader(os.Stdin)}
+}
+
+func (r Reader) String() string {
+	line, _ := reader.r.ReadString('\n')
+	return strings.TrimRight(line, "\r\n")
+}
+
+func (r Reader) Int() int {
+	line := r.String()
+	v, _ := strconv.Atoi(line)
+	return v
+}
+
+func (r Reader) IntPair() (int, int) {
+	v := strings.Split(r.String(), " ")
+	v0, _ := strconv.Atoi(v[0])
+	v1, _ := strconv.Atoi(v[1])
+	return v0, v1
+}
+
+func (r Reader) IntTriple() (int, int, int) {
+	v := strings.Split(r.String(), " ")
+	v0, _ := strconv.Atoi(v[0])
+	v1, _ := strconv.Atoi(v[1])
+	v2, _ := strconv.Atoi(v[2])
+	return v0, v1, v2
+}
+
+func (r Reader) IntQuad() (int, int, int, int) {
+	v := strings.Split(r.String(), " ")
+	v0, _ := strconv.Atoi(v[0])
+	v1, _ := strconv.Atoi(v[1])
+	v2, _ := strconv.Atoi(v[2])
+	v3, _ := strconv.Atoi(v[3])
+	return v0, v1, v2, v3
+}
+
+func (r Reader) Ints(n int) []int {
+	ans := make([]int, 0, n)
+	for _, v := range strings.Split(r.String(), " ") {
+		if v == "" {
+			continue
+		}
+		v, _ := strconv.Atoi(v)
+		ans = append(ans, v)
+	}
+	return ans
+}
+
+func (r Reader) StringPair() (string, string) {
+	v := strings.Split(r.String(), " ")
+	return v[0], v[1]
+}
+
+func (r Reader) Strings(n int) []string {
+	res := make([]string, 0, n)
+	for _, v := range strings.Split(r.String(), " ") {
+		res = append(res, v)
+	}
+	return res
+}
